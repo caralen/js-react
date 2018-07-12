fbc87e16947f498f6b2c7cc655d1a3de1ae8745b
acarin <alen.carin@fer.hr>
2018-07-12 12:32:03 +0200

    Finished listing shows and episodes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(args) {
+    super(args);
+
+    this.state = {
+      shows: [],
+      showDetails: [],
+      showEpisodes: [],
+    };
+    this._onButtonClicked = this._onButtonClicked.bind(this);
+    this._getShowDetails = this._getShowDetails.bind(this);
+    this._getShowEpisodes = this._getShowEpisodes.bind(this);
+  }
+
+  _onButtonClicked() {
+    fetch('https://api.infinum.academy/api/shows')
+	    .then((data) => data.json())
+	    .then((response) => this.setState({ shows: response.data }));
+  }
+
+  _getShowDetails(id) {
+    fetch(`https://api.infinum.academy/api/shows/${id}`)
+	    .then((data) => data.json())
+      .then((response) => this.setState({ showDetails: response.data }));
+      
+    this._getShowEpisodes(id);
+  }
+
+  _getShowEpisodes(id) {
+    fetch(`https://api.infinum.academy/api/shows/${id}/episodes`)
+	    .then((data) => data.json())
+	    .then((response) => this.setState({ showEpisodes: response.data }));
+  }
+
+  _randomColor() {
+    const colors = ['blue', 'green', 'black'];
+    const number = Math.round(Math.random() * 10);
+    return colors[number % 3];
+  }
+
   render() {
+
     return (
-      <h1>
-        Hello Academy!
-      </h1>
+      <div>
+        {
+          this.state.shows.length === 0
+            ? <button onClick={this._onButtonClicked} type="button">
+                TV shows
+              </button>
+            : <p style={{color: 'red'}}>There are {this.state.shows.length} shows.</p>
+        }
+
+        
+        {
+          this.state.shows.map((show) => <button onClick={() => this._getShowDetails(show._id)} type="button">{show.title}</button>)
+        }
+        {
+          <div> <p>{this.state.showDetails.title}</p> <p>{this.state.showDetails.description}</p> </div>
+        }
+        {
+          this.state.showEpisodes.map((episode) => <div> <p>{episode.title}</p> <p>{episode.description}</p> </div>)
+        }
+        
+      </div>
     );
   }
 }
